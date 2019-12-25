28e652e276fa1a0e7b99a6606dd07b03673798d3
Hasa Elman <elman.hasa@ynap.com>
2019-12-25 23:11:07 +0100

    Add solution to problem 2

--- src/com/hasael/solutions/Longest_Substring_Without_Repeating_Characters.java
+++ src/com/hasael/solutions/Longest_Substring_Without_Repeating_Characters.java
@@ -2,13 +2,45 @@ package com.hasael.solutions;
 
 import com.hasael.common.Solution;
 
+/*
+Given a string, find the length of the longest substring without repeating characters. Here is an example solution in Python language.
+
+    class Solution:
+      def lengthOfLongestSubstring(self, s):
+        # Fill this in.
+
+    print Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx')
+    # 10
+
+Can you find a solution in linear time?
+ */
 public class Longest_Substring_Without_Repeating_Characters implements Solution {
     @Override
     public Object solution(Object input) {
-        return innerSolution((String)input);
+        return innerSolution((String) input);
     }
 
-    private int innerSolution(String text){
-        return 2;
+    private int innerSolution(String text) {
+        //Have a max count
+        //Have a current count
+        //Iterate through the text
+        //Increase current count at each iteration
+        //If current char is repeated and current count > max count, update max
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int i = 0; i < text.length() - 1; i++) {
+            char c = text.charAt(i);
+            char next = text.charAt(i + 1);
+            currentCount++;
+            if (c == next) {
+                if (maxCount < currentCount) {
+                    maxCount = currentCount;
+                }
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
     }
 }
