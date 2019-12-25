a74b8fb7a17dd6176927739444448e892283c396
Hasa Elman <elman.hasa@ynap.com>
2019-12-25 22:54:10 +0100

    Solution to problem 1

--- src/com/hasael/solutions/Add_Two_Numbers_As_LinkedList.java
+++ src/com/hasael/solutions/Add_Two_Numbers_As_LinkedList.java
@@ -2,10 +2,17 @@ package com.hasael.solutions;
 
 import com.hasael.common.Solution;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/*
+You are given two linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contains a single digit. Add the two numbers and return them as a linked list. Example:
+
+    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    Output: 7 -> 0 -> 8
+    Explanation: 342 + 465 = 807.
+ */
 public class Add_Two_Numbers_As_LinkedList implements Solution {
     @Override
     public Object solution(Object input) {
@@ -14,6 +21,37 @@ public class Add_Two_Numbers_As_LinkedList implements Solution {
     }
 
     private LinkedList<Integer> innerSolution(LinkedList<Integer> first, LinkedList<Integer> second) {
-        return first;
+        //Obtain first and second number
+        int firstNum = numberFromList(first);
+        int secondNum = numberFromList(second);
+        int result = firstNum + secondNum;
+        //Insert sum to linked list
+        return listFromNumber(result);
+    }
+
+    private LinkedList<Integer> listFromNumber(int number) {
+        int current = number;
+        LinkedList<Integer> result = new LinkedList<>();
+        //divide by powers of 10 to obtain digits
+
+        while (current > 0) {
+            int digit = (current % 10);
+            result.add(digit);
+            current = current / 10;
+        }
+        return result;
+    }
+
+    //Return number from list. Zero if no items in list
+    private int numberFromList(LinkedList<Integer> list) {
+        //Obtain digits to array
+        int number = 0;
+        int found = 0;
+        while (list.peek() != null) {
+            number += list.peek() * Math.pow(10, found);
+            found++;
+            list.pop();
+        }
+        return number;
     }
 }
