fd668178a2438273feabac4647bab511a92cf890
Hasa Elman <elman.hasa@ynap.com>
2019-12-25 23:00:23 +0100

    Add problem 2

--- src/com/hasael/ExercisesDictionary.java
+++ src/com/hasael/ExercisesDictionary.java
@@ -3,6 +3,7 @@ package com.hasael;
 import com.hasael.common.Exercise;
 import com.hasael.solutions.Add_Two_Numbers_As_LinkedList;
 import com.hasael.solutions.HelloWorld;
+import com.hasael.solutions.Longest_Substring_Without_Repeating_Characters;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -22,6 +23,12 @@ public class ExercisesDictionary {
                     "You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.",
                     Arrays.asList(Arrays.asList(new LinkedList<Integer>(Arrays.asList(2,4,3)),new LinkedList<Integer>(Arrays.asList(5,6,4)))),
                     Arrays.asList(new LinkedList(Arrays.asList(7,0,8))),
-                    x -> new Add_Two_Numbers_As_LinkedList().solution(x))
+                    x -> new Add_Two_Numbers_As_LinkedList().solution(x)),
+            new Exercise(
+                    "Longest Substring Without Repeating Characters",
+                    "Given a string, find the length of the longest substring without repeating characters. Here is an example solution in Python language.",
+                    Arrays.asList("abrkaabcdefghijjxxx"),
+                    Arrays.asList(10),
+                    x -> new Longest_Substring_Without_Repeating_Characters().solution(x))
     );
 }
---
+++ src/com/hasael/solutions/Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,14 @@
+package com.hasael.solutions;
+
+import com.hasael.common.Solution;
+
+public class Longest_Substring_Without_Repeating_Characters implements Solution {
+    @Override
+    public Object solution(Object input) {
+        return innerSolution((String)input);
+    }
+
+    private int innerSolution(String text){
+        return 2;
+    }
+}
