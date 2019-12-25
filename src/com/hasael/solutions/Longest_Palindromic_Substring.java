6f7657f24522cff00b1d37ce5ebb6a158844b622
Hasa Elman <elman.hasa@ynap.com>
2019-12-25 23:44:30 +0100

    Add solution to problem 3

--- src/com/hasael/solutions/Longest_Palindromic_Substring.java
+++ src/com/hasael/solutions/Longest_Palindromic_Substring.java
@@ -2,6 +2,10 @@ package com.hasael.solutions;
 
 import com.hasael.common.Solution;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Longest_Palindromic_Substring implements Solution {
     @Override
     public Object solution(Object input) {
@@ -9,6 +13,32 @@ public class Longest_Palindromic_Substring implements Solution {
     }
 
     private String innerSolution(String text) {
-        return "hello";
+        text = text.toLowerCase();
+        //iterate through all combination of the list
+        // have two cursors one from left one from right
+        //the left cursor will move left after the right cursor has reached it
+        //A substring between two cursors will be checked if is palindrome
+        //save all palindrome strings to array
+        //return the longest
+        List<String> palindromes = new ArrayList<>();
+        for (int i = 0; i < text.length() - 1; i++) {
+            for (int j = text.length(); j > i; j--) {
+                String subStr = text.substring(i, j);
+                if (isPalindrome(subStr))
+                    palindromes.add(subStr);
+            }
+        }
+        if (palindromes.size() > 0) {
+            return palindromes.stream().max(Comparator.comparingInt(String::length)).orElse("");
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String text) {
+        StringBuilder reverse = new StringBuilder();
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reverse.append(text.charAt(i));
+        }
+        return text.equals(reverse.toString());
     }
 }
