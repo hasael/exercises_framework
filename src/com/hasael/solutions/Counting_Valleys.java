d8d75e08e8b7fb3c1e6b23c7e0568c11360ebdb2
Hasa Elman <elman.hasa@ynap.com>
2019-12-26 13:32:12 +0100

    Add solution to counting valleys

--- src/com/hasael/solutions/Counting_Valleys.java
+++ src/com/hasael/solutions/Counting_Valleys.java
@@ -9,6 +9,23 @@ public class Counting_Valleys implements Solution {
     }
 
     private int countingValleys(String s) {
-        return 2;
+        //consider D as -1 and U as +1
+        //keep a sum of each step to account for current altitude
+        //increase a counter each time the altitude goes from negative to zero
+        int sum = 0;
+        int valleyCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int step = 0;
+            if (c == 'D')
+                step = -1;
+            else if (c == 'U')
+                step = 1;
+            if (sum < 0 && sum + step >= 0) {
+                valleyCount++;
+            }
+            sum += step;
+        }
+        return valleyCount;
     }
 }
